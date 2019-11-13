bb3c45cf0c4984e6f4a8ce09cf5e24ebc626a686
LastStep <drohanyadav@gmail.com>
2019-11-14 04:13:09 +0530

    dbdwebsite

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+import json
+import os
+from app import app
+from flask import render_template, url_for
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_STATIC = os.path.join(APP_ROOT, 'static')
+
+with open(os.path.join(APP_STATIC, 'Survivor_Perks.json'), "r") as f:
+    survivor_perks = json.load(f)
+
+with open(os.path.join(APP_STATIC, 'Killer_Perks.json'), "r") as f:
+    killer_perks = json.load(f)
+
+with open(os.path.join(APP_STATIC, 'Killer_Addons.json'), "r") as f:
+    killer_addons = json.load(f)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',
+                           Perks={'Survivor-Perks': survivor_perks, 'Killer-Perks': killer_perks}, 
+                           Killer_Addons=killer_addons, css='home.css')
+
+
+@app.route("/analysis")
+def analysis():
+    return render_template('analysis.html', perks=survivor_perks, css='analysis.css')
