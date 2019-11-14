161620452b20410a7426e90da7430c1cc7510ab3
LastStep <drohanyadav@gmail.com>
2019-11-14 22:19:56 +0530

    'dbdwebsite'

--- app/routes.py
+++ app/routes.py
@@ -15,12 +15,16 @@ with open(os.path.join(APP_STATIC, 'Killer_Perks.json'), "r") as f:
 with open(os.path.join(APP_STATIC, 'Killer_Addons.json'), "r") as f:
     killer_addons = json.load(f)
 
+with open(os.path.join(APP_STATIC, 'Items.json'), "r") as f:
+    items = json.load(f)
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html',
                            Perks={'Survivor-Perks': survivor_perks, 'Killer-Perks': killer_perks}, 
-                           Killer_Addons=killer_addons, css='home.css')
+                           Killer_Addons=killer_addons,
+                           Items=items, css='home.css')
 
 
 @app.route("/analysis")
